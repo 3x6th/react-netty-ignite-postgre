fb8817ac30e66206bdaff624f9fc7a55cdf5fac3
Nikita Perevalov <naperevalov@spasibosb.ru>
2025-09-27 14:22:13 +0300

    init commit

---
+++ src/main/java/ru/spasibo/react_netty_ignite_postgre/ReactNettyIgnitePostgreApplication.java
@@ -0,0 +1,13 @@
+package ru.spasibo.react_netty_ignite_postgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactNettyIgnitePostgreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactNettyIgnitePostgreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/spasibo/react_netty_ignite_postgre/ReactNettyIgnitePostgreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.spasibo.react_netty_ignite_postgre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactNettyIgnitePostgreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
